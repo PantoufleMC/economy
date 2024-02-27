4f52d74de86ab6a66761863187aaf9f30bc712c0
sehnryr <youn@melois.dev>
2024-02-27 15:09:32 +0100

    feat: implement economy.balance command

--- src/main/java/org/pantouflemc/economy/Economy.java
+++ src/main/java/org/pantouflemc/economy/Economy.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.pantouflemc.economy.commands.EconomyBalance;
 
 import org.pantouflemc.economy.database.DatabaseManager;
 
@@ -25,7 +26,7 @@ public final class Economy extends JavaPlugin {
 
         // Register commands
         this.getCommand("economy").setExecutor(new org.pantouflemc.economy.commands.Economy());
-        this.getCommand("economy.balance").setExecutor(new org.pantouflemc.economy.commands.EconomyBalance());
+        this.getCommand("economy.balance").setExecutor(new EconomyBalance(databaseManager));
         this.getCommand("economy.pay").setExecutor(new org.pantouflemc.economy.commands.EconomyPay());
         this.getCommand("economy.set").setExecutor(new org.pantouflemc.economy.commands.EconomySet());
         this.getCommand("economy.add").setExecutor(new org.pantouflemc.economy.commands.EconomyAdd());
--- src/main/java/org/pantouflemc/economy/commands/EconomyBalance.java
+++ src/main/java/org/pantouflemc/economy/commands/EconomyBalance.java
@@ -1,17 +1,48 @@
 package org.pantouflemc.economy.commands;
 
+import java.util.UUID;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import org.pantouflemc.economy.database.DatabaseManager;
+
 public class EconomyBalance implements CommandExecutor {
+
+    private final DatabaseManager databaseManager;
+
+    public EconomyBalance(DatabaseManager databaseManager) {
+        this.databaseManager = databaseManager;
+    }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
             @NotNull String[] args) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onCommand'");
+        // Check if the sender is a player
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+
+        // Get the player
+        Player player = (Player) sender;
+
+        // Get the balance of the player
+        UUID playerUuid = player.getUniqueId();
+        try {
+            Integer mainPlayerAccountId = this.databaseManager.getMainAccount(playerUuid);
+            assert mainPlayerAccountId != null : "Player does not have a main account";
+
+            @SuppressWarnings("null")
+            double balance = this.databaseManager.getBalance(mainPlayerAccountId);
+            player.sendMessage("Your balance is $" + balance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
     }
-    
+
 }
