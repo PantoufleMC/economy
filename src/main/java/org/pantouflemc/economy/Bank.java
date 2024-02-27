621d35e963d8d380c63c44baeafded08b518d44d
sehnryr <youn@melois.dev>
2024-02-27 01:35:40 +0100

    feat: initial commit
    Setup project using the Minecraft Development plugin from IntelliJ IDEA

---
+++ src/main/java/org/pantouflemc/economy/Bank.java
@@ -0,0 +1,17 @@
+package org.pantouflemc.economy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bank extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
