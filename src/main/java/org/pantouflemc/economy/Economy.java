dd11e9a9992b2b22bdc2b42b1edb1ffe06159c6c
sehnryr <youn@melois.dev>
2024-02-28 01:10:09 +0100

    feat: implement economy.set command

--- src/main/java/org/pantouflemc/economy/Economy.java
+++ src/main/java/org/pantouflemc/economy/Economy.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.pantouflemc.economy.commands.EconomyBalance;
+import org.pantouflemc.economy.commands.EconomySet;
 
 import org.pantouflemc.economy.database.DatabaseManager;
 
@@ -28,7 +29,7 @@ public final class Economy extends JavaPlugin {
         this.getCommand("economy").setExecutor(new org.pantouflemc.economy.commands.Economy());
         this.getCommand("economy.balance").setExecutor(new EconomyBalance(databaseManager));
         this.getCommand("economy.pay").setExecutor(new org.pantouflemc.economy.commands.EconomyPay());
-        this.getCommand("economy.set").setExecutor(new org.pantouflemc.economy.commands.EconomySet());
+        this.getCommand("economy.set").setExecutor(new EconomySet(databaseManager));
         this.getCommand("economy.add").setExecutor(new org.pantouflemc.economy.commands.EconomyAdd());
         this.getCommand("economy.remove").setExecutor(new org.pantouflemc.economy.commands.EconomyRemove());
         this.getCommand("economy.top").setExecutor(new org.pantouflemc.economy.commands.EconomyTop());
--- src/main/java/org/pantouflemc/economy/commands/EconomySet.java
+++ src/main/java/org/pantouflemc/economy/commands/EconomySet.java
@@ -1,17 +1,74 @@
 package org.pantouflemc.economy.commands;
 
+import java.util.UUID;
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import org.pantouflemc.economy.database.DatabaseManager;
+
 public class EconomySet implements CommandExecutor {
+
+    private final DatabaseManager databaseManager;
+
+    public EconomySet(DatabaseManager databaseManager) {
+        this.databaseManager = databaseManager;
+    }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
             @NotNull String[] args) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onCommand'");
+        // Parse the arguments
+        ImmutablePair<String, Double> arguments = this.parseArguments(args);
+        if (arguments == null) {
+            return false;
+        }
+
+        // Get the target player
+        @Nullable
+        OfflinePlayer targetPlayer = sender.getServer().getOfflinePlayerIfCached(arguments.getLeft());
+        if (targetPlayer == null) {
+            sender.sendMessage("Target not found");
+            return false;
+        }
+
+        // Set the balance of the target player
+        double amount = arguments.getRight();
+        UUID targetPlayerUuid = targetPlayer.getUniqueId();
+        try {
+            Integer mainPlayerAccountId = this.databaseManager.getMainAccount(targetPlayerUuid);
+            // assert mainPlayerAccountId != null : "Player does not have a main account";
+            if (mainPlayerAccountId == null) {
+                sender.sendMessage("Player does not have a main account");
+                return false;
+            }
+
+            this.databaseManager.setBalance(mainPlayerAccountId, amount);
+            sender.sendMessage("Balance of " + targetPlayer.getName() + " set to $" + amount);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
     }
-    
+
+    private @Nullable ImmutablePair<String, Double> parseArguments(String[] args) {
+        if (args.length != 2) {
+            return null;
+        }
+
+        try {
+            String playerName = args[0];
+            double amount = Double.parseDouble(args[1]);
+            return new ImmutablePair<>(playerName, amount);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
