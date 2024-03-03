57aac414e709633050faae614a2b50912f39e2a7
sehnryr <youn@melois.dev>
2024-03-03 19:24:36 +0100

    feat: add better formatting for balancetop command

--- src/main/java/org/pantouflemc/economy/commands/EconomyBalanceTopCommand.java
+++ src/main/java/org/pantouflemc/economy/commands/EconomyBalanceTopCommand.java
@@ -1,11 +1,20 @@
 package org.pantouflemc.economy.commands;
 
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.pantouflemc.economy.Economy;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+
 public class EconomyBalanceTopCommand extends EconomyCommandExecutor {
+
+    private final Component messageUsage = Component
+            .text("Usage: /balancetop", NamedTextColor.RED);
 
     public EconomyBalanceTopCommand() {
         super("balancetop");
@@ -15,28 +24,45 @@ public class EconomyBalanceTopCommand extends EconomyCommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
             @NotNull String[] args) {
         if (!sender.hasPermission("economy.balancetop")) {
-            sender.sendMessage("You don't have permission to use this command");
-            return false;
-        }
-
-        try {
-            // Check if the command has no arguments
-            if (args.length != 0) {
-                return false;
-            }
-
-            // Get the accounts
-            var accounts = Economy.getPlugin().getTopPlayerAccounts(10, 0);
-            for (int i = 0; i < accounts.size(); i++) {
-                var account = accounts.get(i);
-                sender.sendMessage((i + 1) + ". " + account.getLeft() + " - $" + account.getRight());
-            }
-
+            sender.sendMessage(messageNoPermission);
             return true;
-        } catch (Exception e) {
-            sender.sendMessage("An error occurred");
-            return false;
         }
+
+        // Check if the command has no arguments
+        if (args.length != 0) {
+            sender.sendMessage(messageUsage);
+            return true;
+        }
+
+        // Get the accounts
+        final @NotNull List<ImmutablePair<String, Double>> accounts;
+        try {
+            accounts = Economy.getPlugin().getTopPlayerAccounts(10, 0);
+        } catch (Exception e) {
+            sender.sendMessage(messageErrorOccurred);
+            return true;
+        }
+
+        // Get the max length of the account names
+        int maxLength = 0;
+        for (final ImmutablePair<String, Double> account : accounts) {
+            maxLength = Math.max(maxLength, account.getLeft().length());
+        }
+
+        // Show the accounts
+        for (int i = 0; i < accounts.size(); i++) {
+            final ImmutablePair<String, Double> account = accounts.get(i);
+
+            final String padding = " ".repeat(maxLength - account.getLeft().length());
+            final Component message = Component.text()
+                    .content((i + 1) + ". ").color(NamedTextColor.GRAY)
+                    .append(Component.text(account.getLeft() + padding, NamedTextColor.WHITE))
+                    .append(Component.text(" - ", NamedTextColor.GRAY))
+                    .append(Component.text("$" + formatCurrency(account.getRight()), NamedTextColor.WHITE))
+                    .build();
+            sender.sendMessage(message);
+        }
+        return true;
     }
 
 }
